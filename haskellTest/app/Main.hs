d79c56fcfe6526c1868dc34bda7c2d3315005ba0
momikimasashi <mngjxd03170317@gmail.com>
2018-07-08 00:58:58 +0900

    すごいH本のためのhsファイル追加

--- haskellTest/app/Main.hs
+++ haskellTest/app/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Lib
 import Prac
+import SHBook
 
 main :: IO ()
 main = do
+  -- Prac Function
   --someFunc
   --print $ fib 6
   --print $ fib_gard 6
@@ -21,4 +23,9 @@ main = do
   --print $ isort [4, 2, 3, 1]
   --print $ show $ testDebug 5 --traceIO‚ÍŽg‚¦‚È‚©‚Á‚½
   --print $ bsort [3, 5, 33, 496, 2, 7, 232]
-  print $ msort [3, 5, 33, 496, 2, 7, 232]
+  --print $ msort [3, 5, 33, 496, 2, 7, 232]
+  
+  --SHBook Function
+  --print $ doubleMe 5
+  --print $ doubleUs 4 9
+  print $ doubleSmallNumber 4
--- haskellTest/src/Prac.hs
+++ haskellTest/src/Prac.hs
@@ -148,3 +148,5 @@ merge xs [] = xs
 merge (x:xs) (y:ys) 
   | x < y = x : merge xs (y:ys)
   | otherwise = y : merge (x:xs) ys
+
+
